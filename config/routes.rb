86f44bcb349421cb47815a09c526a11f2ac253c3
cody <codylocklear82@gmail.com>
2016-10-19 05:26:39 -0400

    commit 5.1

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#landing_page'
+  root 'static_pages#about'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
