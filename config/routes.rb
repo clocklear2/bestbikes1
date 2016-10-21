7f52cc235b19cc301fb726779fd46408e9507a03
cody <codylocklear82@gmail.com>
2016-10-21 00:38:28 -0400

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   root 'static_pages#about'
 
+  post 'static_pages/thank_you'
+
+
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
