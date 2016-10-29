34d474c88b4d980279f06ae80d6a6b70c1b0c49b
cody <codylocklear82@gmail.com>
2016-10-29 16:59:27 -0400

    commit

--- db/migrate/20161028080006_rename_price_in_products.rb
+++
@@ -1,6 +0,0 @@
-class RenamePriceInProducts < ActiveRecord::Migration[5.0]
-
-  def change
-  	rename_column :products, :price, :prices
-  end
-end
--- db/migrate/20161029083241_rename_prices_in_products.rb
+++
@@ -1,6 +0,0 @@
-class RenamePricesInProducts < ActiveRecord::Migration[5.0]
-
-  def change
-  	rename_column :products, :prices, :price
-  end
-end
