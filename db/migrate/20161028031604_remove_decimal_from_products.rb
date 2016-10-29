9b30bb52119cc305fe4ae2a57711ed7b86d385a5
cody <codylocklear82@gmail.com>
2016-10-29 04:18:22 -0400

    commit

--- db/migrate/20161028031604_remove_decimal_from_products.rb
+++ db/migrate/20161028031604_remove_decimal_from_products.rb
@@ -1,6 +1,5 @@
 class RemoveDecimalFromProducts < ActiveRecord::Migration[5.0]
   def change
     remove_column :products, :decimal, :string
-    remove_column :products, :decimal, :string
   end
 end
