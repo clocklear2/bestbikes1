1104ab360e3dee42f70076a6b37ad77e0a5237bc
cody <codylocklear82@gmail.com>
2016-10-29 04:36:19 -0400

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028080006) do
+ActiveRecord::Schema.define(version: 20161029083241) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20161028080006) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
-    t.decimal  "prices"
+    t.decimal  "price"
   end
 
   create_table "users", force: :cascade do |t|
