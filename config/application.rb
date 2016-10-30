8030a7a47a4226f028d0a57e6fcd4308bcb0a385
cody <codylocklear82@gmail.com>
2016-10-30 03:09:12 -0400

    commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 
 module Mybikeproject
   class Application < Rails::Application
