fa332686197a0141563290c0e219979c04b19db4
cody <codylocklear82@gmail.com>
2016-10-30 03:07:05 -0400

    co

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 module Mybikeproject
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
